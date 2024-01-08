a53c266322a8e6bb5a9c5fe7cc623faf19640bae
mhmdfathy96 <mhmdfathy96@gmail.com>
2024-01-08 23:50:05 +0200

    0

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  publicPath: process.env.BASE_URL === "production" ? "/installment/" : "/",
   plugins: [
     vue(),
     vueJsx(),
