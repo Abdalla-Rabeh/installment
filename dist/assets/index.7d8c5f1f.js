13451d9396ff141a030bb3d88e059ebdcfaa0b4f
mhmdfathy96 <mhmdfathy96@gmail.com>
2024-01-09 19:55:41 +0200

    test

--- dist/assets/index.7d8c5f1f.js
+++
@@ -1,1 +0,0 @@
-import{ba as t}from"./index.70035ea2.js";const a=t("ClientDataShera",{state:()=>({ClientData:{},text:"Hi"})});export{a as G};
---
+++ dist/assets/index.94bda6b5.js
@@ -0,0 +1,1 @@
+import{ba as t}from"./index.f9aef19c.js";const a=t("ClientDataShera",{state:()=>({ClientData:{},text:"Hi"})});export{a as G};
